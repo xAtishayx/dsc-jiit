03e473391d05c776d4b608aa028c823ab8574e46
Shivansh Gupta <shiv0403gupta@gmail.com>
2021-01-15 03:36:45 +0530

    youtube section added

---
+++ .history/pages/video_20210115033304.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar2 from "../components/navbar2";
+import Contact from "../components/contact";
+
+export default function Videos() {
+    return (
+      <div className="header-3">
+        <Navbar2 />
+        <Contact />
+      </div>
+    );
+  }
